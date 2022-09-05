552f2b8b5bca9975ef1815c996c0c76244ae295d
Vladimir1984g <108072954+Vladimir1984g@users.noreply.github.com>
2022-09-05 18:01:08 +0900

    fist commit

---
+++ main.js
@@ -0,0 +1,18 @@
+$(function(){
+    $('.design-slider').slick({
+        dots: false,
+        slidesToShow: 4,
+        variableWidth: true,
+        prevArrow: '<img class="arrow arrow-left" src="arrow-left.svg" alt="">',
+        nextArrow: '<img class="arrow arrow-right" src="arrow-right.svg" alt="">',
+        responsive: [
+            {
+                breakpoint: 361,
+                settings: {
+                    variableWidth: false,
+                    slidesToShow: 1,
+                }
+            }
+        ]
+    });
+});
